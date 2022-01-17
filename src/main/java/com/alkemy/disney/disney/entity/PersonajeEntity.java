a5057189fc82fdd43b90f87455afd7df6bf264a9
JoseMa35 <josemcastilloc@gmail.com>
2022-01-17 18:12:32 -0300

    entitySet

---
+++ src/main/java/com/alkemy/disney/disney/entity/PersonajeEntity.java
@@ -0,0 +1,32 @@
+package com.alkemy.disney.disney.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "personaje")
+@Getter
+@Setter
+public class PersonajeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    private String nombre;
+
+    private Long edad;
+
+    private Long peso;
+
+    private String historia;
+
+    @ManyToMany(mappedBy = "personajes", cascade = CascadeType.ALL)
+    private List<PeliculaSerieEntity> pelicualas = new ArrayList<>();
+
+
+}
