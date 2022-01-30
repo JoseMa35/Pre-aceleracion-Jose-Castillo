b54da46d4235a7f9e7b78232cca6fdd8153c13ad
JoseMa35 <josemcastilloc@gmail.com>
2022-01-30 18:56:56 -0300

    overrideErrorExeptions

--- src/main/java/com/alkemy/disney/disney/controller/RestExceptionHandler.java
+++ src/main/java/com/alkemy/disney/disney/controller/RestExceptionHandler.java
@@ -5,12 +5,17 @@ import com.alkemy.disney.disney.exception.ParamNotFound;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 @ControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
@@ -23,5 +28,24 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
                 Arrays.asList("Param Not Found")
         );
         return handleExceptionInternal(ex,errorDTO, new HttpHeaders(),HttpStatus.BAD_REQUEST, request);
+    }
+    //TODO ver el @Override más adelante.
+    //@Override
+    protected ResponseEntity<Object> handlerMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            HttpHeaders headers,
+            HttpStatus status,
+            WebRequest request){
+        List<String> errors = new ArrayList<>();
+        for (FieldError error : ex.getBindingResult().getFieldErrors()){
+            errors.add(error.getField() + ": " + error.getDefaultMessage());
+        }
+        for (ObjectError error : ex.getBindingResult().getFieldErrors()){
+            errors.add(error.getObjectName() + ": " + error.getDefaultMessage());
+        }
+        ApiErrorDTO apiError = new ApiErrorDTO(HttpStatus.BAD_REQUEST, ex.getLocalizedMessage(),errors);
+
+        return handleExceptionInternal(
+                ex,apiError, headers,apiError.getStatus(), request);
     }
 }
